78fc8835337a2a1ecb163b50d3960c5aebb295f4
Arnaldo Martins Hidalgo Junior <arnaldo@hidalgojunior.com.br>
2023-11-01 13:56:56 -0300

    Etapa2

--- apresentacao.php
+++ apresentacao.php
@@ -4,37 +4,47 @@
 <div class="row">
     <div class="col-12">
         <h1>Apresentação</h1>
-        <p>TCC Apresentado aos professores de Segurança do Trabalho da Etec Antonio Devisate de Marília.</p>
+        <p>Trabalho de conclusão de curso apresentado ao curso de Técnico em Segurança do Trabalho da ETEC Antônio Devisate , orientado pelo prof. Eduardo Batistteti, como requisito parcial para a obtenção do título de Técnico em Segurança do Trabalho. </p>
     </div>
 </div>
     <div class="row">
-        <div class="col-lg-4 col-md-6 col-sm-12">
-            <div class="card ">
-                <div class="card-title bg-primary">
-                    Nome do Aluno
-                </div>
+        <div class="col-lg-3 col-md-6 col-sm-12">
+            <div class="card" style="width: 18rem;">
+                <img src="imgs/men04.png" class="card-img-top" alt="...">
                 <div class="card-body">
-                    Infromações
+                    <h5 class="card-title">KERVELIN DE SOUZA</h5>
+                    <p class="card-text">Aluno do segundo módulo de Segurança do Trabalho da Etec Antonio Devisate</p>
+                    <a href="mailto:kervelin.souza@etec.sp.gov.br" class="btn btn-primary">Enviar e-mail</a>
                 </div>
             </div>
         </div>
-        <div class="col-lg-4 col-md-6 col-sm-12">
-            <div class="card">
-                <div class="card-title bg-primary">
-                    Nome do Aluno
-                </div>
+        <div class="col-lg-3 col-md-6 col-sm-12">
+            <div class="card" style="width: 18rem;">
+                <img src="imgs/men04.png" class="card-img-top" alt="...">
                 <div class="card-body">
-                    Infromações
+                    <h5 class="card-title">MATHEUS SERRANO</h5>
+                    <p class="card-text">Aluno do segundo módulo de Segurança do Trabalho da Etec Antonio Devisate</p>
+                    <a href="mailto:matheus.serrano@etec.sp.gov.br" class="btn btn-primary">Enviar e-mail</a>
                 </div>
             </div>
         </div>
-        <div class="col-lg-4 col-md-6 col-sm-12">
-            <div class="card">
-                <div class="card-title bg-primary">
-                    Nome do Aluno
-                </div>
+        <div class="col-lg-3 col-md-6 col-sm-12">
+            <div class="card" style="width: 18rem;">
+                <img src="imgs/men04.png" class="card-img-top" alt="...">
                 <div class="card-body">
-                    Infromações
+                    <h5 class="card-title">RAFAEL MARTIN</h5>
+                    <p class="card-text">Aluno do segundo módulo de Segurança do Trabalho da Etec Antonio Devisate</p>
+                    <a href="mailto:rafael.martin01@etec.sp.gov.br" class="btn btn-primary">Enviar e-mail</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-12">
+            <div class="card" style="width: 18rem;">
+                <img src="imgs/men04.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">HENRIQUE SANTOS</h5>
+                    <p class="card-text">Aluno do segundo módulo de Segurança do Trabalho da Etec Antonio Devisate</p>
+                    <a href="mailto:henrique.santos464@etec.sp.gov.br" class="btn btn-primary">Enviar e-mail</a>
                 </div>
             </div>
         </div>
